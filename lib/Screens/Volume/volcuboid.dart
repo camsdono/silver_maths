0341847e3ba9ec931570ad7eed833bfcb6fe7714
camsdono <68128526+camsdono@users.noreply.github.com>
2021-10-27 20:32:02 +0100

    Update volcuboid.dart

--- lib/Screens/Volume/volcuboid.dart
+++ lib/Screens/Volume/volcuboid.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 import 'dart:core';
 import 'package:flutter/services.dart';
-import 'package:angles/angles.dart';
 
 
 void main() => runApp(const VolCubiodWorkingScreen());
@@ -21,35 +20,53 @@ class VolCubiodWorkingScreen extends StatefulWidget {
 class _MyCustomFormState extends State<VolCubiodWorkingScreen> {
   // Create a text controller and use it to retrieve the current value
   // of the TextField.
-  final oppositeController = TextEditingController();
-  final adjacentController = TextEditingController();
-
+  final length1Controller = TextEditingController();
+  final breadth1Controller = TextEditingController();
+  final length2Controller = TextEditingController();
+  final breadth2Controller = TextEditingController();
+  final length3Controller = TextEditingController();
+  final breadth3Controller = TextEditingController();
 
   @override
   void dispose() {
     // Clean up the controller when the widget is disposed.
-    adjacentController.dispose();
-    oppositeController.dispose();
+    length1Controller.dispose();
+    breadth1Controller.dispose();
+    length2Controller.dispose();
+    breadth2Controller.dispose();
+    length3Controller.dispose();
+    breadth3Controller.dispose();
     super.dispose();
   }
   var answer = 0.0;
   var answer2 = 0.0;
-  var opposite = 0.0;
-  var adjacent = 0.0;
-  var working1 = 0.0;
-  var working2 = 0.0;
+  var length1 = 0.0;
+  var length2 = 0.0;
+  var length3 = 0.0;
+  var breadth1 = 0.0;
+  var breadth2 = 0.0;
+  var breadth3 = 0.0;
+  var a1 = 0.0;
+  var a2 = 0.0;
+  var a3 = 0.0;
+
 
   void calculate() {
     //Calculate What needs to be calculated
-    opposite = double.parse(oppositeController.text);
-    adjacent = double.parse(adjacentController.text);
-    answer = opposite * opposite + adjacent * adjacent;
-    answer.toString();
-    answer2 = sqrt(answer);
-    answer2.toString();
+    length1 = double.parse(length1Controller.text);
+    length2 = double.parse(length2Controller.text);
+    length3 = double.parse(length3Controller.text);
+    breadth1 = double.parse(breadth1Controller.text);
+    breadth2 = double.parse(breadth2Controller.text);
+    breadth3 = double.parse(breadth3Controller.text);
 
-    working1 = opposite * opposite;
-    working2 = adjacent * adjacent;
+    a1 = length1 * breadth1;
+    a2 = length2 * breadth2;
+    a3 = length3 * breadth3;
+    answer = a1 + a2 + a3;
+    answer2 = answer * 2;
+    answer.toString();
+    answer2.toString();
   }
 
 
@@ -61,89 +78,119 @@ class _MyCustomFormState extends State<VolCubiodWorkingScreen> {
     //answer = adjacent * angle;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pythagoras Long Side'),
+        title: const Text('Cuboid Surface Area'),
       ),
       body: Padding(
-        child: Column (
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: Column(
           children: <Widget>[
-            const Flexible(
-              child: Text(
-                "Length 1"
-              )
+            const Text (
+              "Length 1",
+              textAlign: TextAlign.left,
             ),
-            Flexible(
-              child: TextField(
-                controller: oppositeController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'What is the opposite?',
-                  filled: true,
-                  fillColor: Colors.black,
-                  hintStyle: TextStyle(color: Colors.white),
-                ),
-                keyboardType: TextInputType.number,
+            TextFormField(
+              controller: length1Controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'What is the length of the first side?',
+                filled: true,
+                fillColor: Colors.black,
+                hintStyle: TextStyle(color: Colors.white),
               ),
             ),
-            const SizedBox(height: 10),
-
-            Flexible(
-              child: TextField(
-                controller: adjacentController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'What is the adjacent?',
-                  filled: true,
-                  fillColor: Colors.black,
-                  hintStyle: TextStyle(color: Colors.white),
-                ),
-                keyboardType: TextInputType.number,
+            const SizedBox(height: 5),
+            TextFormField(
+              controller: breadth1Controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'What is the breadth of the first side?',
+                filled: true,
+                fillColor: Colors.black,
+                hintStyle: TextStyle(color: Colors.white),
               ),
             ),
-            const SizedBox(height: 450),
-
-            Flexible(
-              child: FloatingActionButton(
-                // When the user presses the button, show an alert dialog containing
-                // the text that the user has entered into the text field.
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      calculate();
-                      return AlertDialog(
-                          backgroundColor: Colors.white,
-                          content: Text(
-                            oppositeController.text + "²" + " + " + adjacentController.text + "²" + " = x²"  + "\n" +
-                                working1.toString() + " + " + working2.toString() + " = x²" + "\n" +
-                                answer.toString() + " = x²" + "\n" +
-                                "√" + answer.toString() + " = x²" + "\n" +
-                                "x = " + answer2.toStringAsFixed(1),
-                            style: const TextStyle(color: Colors.black),
-                          )
-                      );
-                    },
-                  );
-
-                },
-
-                tooltip: 'Work Out The Total',
-                child: const Text(
-                  '=',
-                  style: TextStyle(
-                    fontSize: 40.0, // insert your font size here
-                  ),
-                ),
+            const SizedBox(height: 5),
+            const Text (
+              "Length 2",
+              textAlign: TextAlign.left,
+            ),
+            TextFormField(
+              controller: length2Controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'What is the length of the second side?',
+                filled: true,
+                fillColor: Colors.black,
+                hintStyle: TextStyle(color: Colors.white),
               ),
-            )
+            ),
+            const SizedBox(height: 5),
+            TextFormField(
+              controller: breadth2Controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'What is the breadth of the second side?',
+                filled: true,
+                fillColor: Colors.black,
+                hintStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+            const SizedBox(height: 5),
+            const Text (
+              "Length 3",
+              textAlign: TextAlign.left,
+            ),
+            TextFormField(
+              controller: length3Controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'What is the length of the third side?',
+                filled: true,
+                fillColor: Colors.black,
+                hintStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+            const SizedBox(height: 5),
+            TextFormField(
+              controller: breadth3Controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'What is the breadth of the third side?',
+                filled: true,
+                fillColor: Colors.black,
+                hintStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+             FloatingActionButton(
+               onPressed: () {
+                 showDialog(
+                   context: context,
+                   builder: (context) {
+                     calculate();
+                     return AlertDialog(
+                         backgroundColor: Colors.white,
+                         content: Text(
+                           answer.toString(),
+                           style: const TextStyle(color: Colors.black),
+                         )
+                     );
+                   },
+                 );
+
+               },
+               tooltip: 'Work Out The Total',
+               child: const Text(
+                 '=',
+                 style: TextStyle(
+                   fontSize: 40.0, // insert your font size here
+                 ),
+               ),
+
+            ),
+
           ],
-
         ),
-        padding: const EdgeInsets.all(16.0),
-
       ),
-
     );
-
   }
 }
